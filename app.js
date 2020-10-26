f7fdabb6a9970d956df5d6ddb10751d1bdff19d6
Sricharan Ramesh <charan1952001@gmail.com>
2020-10-26 21:22:37 +0530

    Upload feature

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+
+const imageRouter = require('./routes/imageRouter');
+const audioRouter = require('./routes/audioRouter');
+const videoRouter = require('./routes/videoRouter');
+
+app.use(morgan('dev'));
+app.use(bodyparser.json());
+
+app.use('/imageUpload',imageRouter);
+app.use('/audioUpload',audioRouter);
+app.use('/videoUpload',videoRouter);
+
+app.use((req,res,next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message
+    });
+});
+
+module.exports = app;
